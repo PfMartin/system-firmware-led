af139c521ffb4669423d605750d2178f00e0c57c
PfMartin <martinpfatrisch@gmail.com>
2024-08-07 21:39:16 +0200

    Established wifi connection

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+use anyhow::Result;
+use esp_idf_hal::prelude::Peripherals;
+use esp_idf_svc::eventloop::EspSystemEventLoop;
+use log::info;
+use std::{thread::sleep, time::Duration};
+use wifi::connect_to_wifi;
 
-    // Bind the log crate to the ESP Logging facilities
+mod wifi;
+
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+    #[default(6)]
+    led_strip_gpio: u32,
+    #[default(8)]
+    indicator_led_gpio: u32,
+}
+
+fn main() -> Result<()> {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    let app_config = CONFIG;
+
+    let peripherals = Peripherals::take()?;
+    let sysloop = EspSystemEventLoop::take()?;
+
+    let _wifi_connection = connect_to_wifi(
+        app_config.wifi_ssid,
+        app_config.wifi_psk,
+        peripherals.modem,
+        sysloop,
+    )?;
+
+    loop {
+        info!("Waiting for messages...");
+        sleep(Duration::from_millis(1000))
+    }
 }
---
+++ src/wifi.rs
@@ -0,0 +1,87 @@
+use anyhow::{bail, Result};
+use embedded_svc::wifi::{
+    AccessPointConfiguration, AuthMethod, ClientConfiguration, Configuration,
+};
+use esp_idf_hal::{modem::Modem, peripheral::Peripheral};
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::BlockingWifi, wifi::EspWifi,
+};
+use heapless::String;
+use log::info;
+
+pub fn connect_to_wifi(
+    ssid: &str,
+    pwd: &str,
+    modem: impl Peripheral<P = Modem> + 'static,
+    sysloop: EspSystemEventLoop,
+) -> Result<Box<EspWifi<'static>>> {
+    info!("Starting wifi connection process");
+
+    check_credentials_not_empty(ssid, pwd)?;
+
+    let nvs = EspDefaultNvsPartition::take()?;
+    let mut esp_wifi = EspWifi::new(modem, sysloop.clone(), Some(nvs))?;
+    let mut wifi = BlockingWifi::wrap(&mut esp_wifi, sysloop)?;
+
+    let default_conf = Configuration::Client(ClientConfiguration::default());
+    wifi.set_configuration(&default_conf)?;
+
+    info!("Starting WiFi");
+    wifi.start()?;
+
+    info!("Scanning...");
+    let ap_infos = wifi.scan()?;
+    let ap_match = ap_infos.into_iter().find(|a| a.ssid == ssid);
+
+    let channel = if let Some(ap_match) = ap_match {
+        info!(
+            "Found configured access point {} on channel {}",
+            ssid, ap_match.channel
+        );
+        Some(ap_match.channel)
+    } else {
+        info!(
+            "Configured access point {} not found during scanning, will go with unknown channel",
+            ssid
+        );
+        None
+    };
+
+    wifi.set_configuration(&Configuration::Mixed(
+        ClientConfiguration {
+            ssid: String::<32>::try_from(ssid).unwrap(),
+            password: String::<64>::try_from(pwd).unwrap(),
+            channel,
+            auth_method: AuthMethod::WPA2Personal,
+            ..Default::default()
+        },
+        AccessPointConfiguration {
+            ssid: String::<32>::try_from("aptest").unwrap(),
+            channel: channel.unwrap_or(1),
+            ..Default::default()
+        },
+    ))?;
+
+    info!("Connecting WiFi...");
+    wifi.connect()?;
+
+    info!("Waiting for DHCP lease...");
+    wifi.wait_netif_up()?;
+
+    let ip_info = wifi.wifi().sta_netif().get_ip_info()?;
+    info!("Connected to WiFi: {:?}", ip_info);
+
+    Ok(Box::new(esp_wifi))
+}
+
+fn check_credentials_not_empty(ssid: &str, pwd: &str) -> Result<()> {
+    if ssid.is_empty() {
+        bail!("Missing WiFi name");
+    }
+
+    if pwd.is_empty() {
+        bail!("Missing WiFi password");
+    }
+
+    Ok(())
+}
